6ff3e05da6d03942c9a848c5d8583745c3856cd7
victoria <ivanovavikt78@gmail.com>
2026-07-01 10:24:16 +0300

    добавлен файл ui_main.py и изменен main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from ui_main import Ui_MainWindow
 
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName('Учёт художественных материалов')
